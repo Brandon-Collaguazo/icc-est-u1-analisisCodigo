6070f4c2f97b2509fb54218466dc520024cd32ec
Brandon <bcollaguazos@est.ups.edu.ec>
2025-05-07 09:01:43 -0500

    Implementacion Grafica

--- src_python/app.py
+++ src_python/app.py
@@ -2,22 +2,52 @@
 # import benchmarking as Ben
 from benchmarking import Benchmarking
 from metodos_ordenamiento import MetodosOrdenamiento
+import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
     print("Funciona")
-    metodos = MetodosOrdenamiento()
+    m_ordenamiento = MetodosOrdenamiento()
     benchmark = Benchmarking()
-    tamanio = 10000
-    arreglo = benchmark.buildArreglo(tamanio)
-    metodos = {
-        'Burbuja': metodos.sortByBubble,
-        'Seleccion': metodos.sort_seleccion,
-    }
+    #tamanio = 10000
+    tamanios = [500, 1000, 2000]
     resultados = []
-    for nombre, metodo in metodos.items():
-        tiempo = benchmark.medir_tiempo(metodo, arreglo)
-        tuplaResultado = (tamanio, nombre, tiempo)
-        resultados.append(tuplaResultado)
-    for resultado in resultados:
-        tamanio, nombre, tiempo = resultado
-        print(f'Tamaño: {tamanio}, Método: {nombre}, Tiempo: {tiempo:.6f} segundos')
+    for tam in tamanios:
+        arreglo = benchmark.buildArreglo(tam)
+        metodos = {
+            'Burbuja': m_ordenamiento.sortByBubble,
+            'Seleccion': m_ordenamiento.sort_seleccion,
+    }
+        
+        for nombre, metodo in metodos.items():
+            tiempo = benchmark.medir_tiempo(metodo, arreglo)
+            tuplaResultado = (tam, nombre, tiempo)
+            resultados.append(tuplaResultado)
+            
+        for resultado in resultados:
+            tamanio, nombre, tiempo = resultado
+            print(f'Tamaño: {tamanios}, Método: {nombre}, Tiempo: {tiempo:.6f} segundos')
+    
+    tiempos_by_metodo = {
+        "Burbuja" : [],
+        "Seleccion" : [],
+    }
+
+    #Clasificar los tiempos segun el método
+    for tam, nombre, tiempo in resultados:
+        tiempos_by_metodo[nombre].append(tiempo)
+
+    #Crear una gráfica
+    plt.figure(figsize=(10, 6))
+
+    #Graficar una linea de tiempo para cada de tiempo
+    #Eje y sean los tiempos obtenidos
+    #Eje x sea el tamaño del arreglo
+    for nombre, tiempo in tiempos_by_metodo.items():
+        plt.plot(tamanios, tiempo, label=nombre, marker='o')
+    
+    #Agregar parametros
+    plt.title("Análisis de Métodos de Ordenamiento\nBrandon Collaguazo 2025-05-07", fontsize=12)
+    plt.xlabel("Tamaño del arreglo")
+    plt.ylabel("Tiempo")
+    plt.grid()
+    plt.show()
